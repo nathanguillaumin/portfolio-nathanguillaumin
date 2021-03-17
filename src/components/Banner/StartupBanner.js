1a5ec3a0264857f43973c78b9425cff105a91d67
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2021-03-17 12:25:49 +0100

    changes wording for growth hacking

--- src/components/Banner/StartupBanner.js
+++ src/components/Banner/StartupBanner.js
@@ -23,16 +23,13 @@ const StBanner = () => {
           <div className="col-lg-5 offset-lg-1 d-flex align-items-center">
             <Fade bottom cascade>
               <div className="agency_content_two">
-                <h2 className="f_700 f_size_40 l_height50 w_color mb_20">
-                  Nathan Guillaumin, web developer
+                <h2 className="f_700 f_size_36 l_height50 w_color mb_20">
+                  Nathan Guillaumin, Web Developer and Growth Hacker.
                 </h2>
                 <p className="f_400 w_color l_height28">
-                  Welcome to my portfolio, I hope you will find all the details
-                  you might need.
-                  <br />
-                  <br />I have specialized in front-end design interfaces using
-                  React.js and SCSS. I also work with back-end technologies such
-                  as Node.js, API Platform and Symfony5.
+                  I have specialized in front-end design interfaces using
+                  React.js as well as in Growth Hacking to help my clients
+                  reaching fast growth.
                 </p>
                 <div className="action_btn d-flex align-items-center mt_40">
                   <Link
